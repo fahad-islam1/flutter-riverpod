ef9d2254b54fc95f79ff7f3c2562ebd994a19202
Fahad islam <fahadch127127@gmail.com>
2025-03-01 10:57:58 +0500

    update

--- lib/user_model.dart
+++ lib/user_model.dart
@@ -3,7 +3,8 @@ class User {
   final int age;
 
   User({required this.name, required this.age});
-// copymethod 
+
+//  copyWith method is used to create a new object with the same properties as the current object, but with some of the properties changed.
   User copyWith({String? name, int? age}) {
     return User(
       name: name ?? this.name,
